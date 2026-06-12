279e8f7cd248ce54d7d8d8182695b105965a1b04
kudawasama <cespedes.jac@gmail.com>
2026-06-12 12:07:50 -0400

    fix: cerrar pop-up Informacion con ESC+Enter (dialogo Windows estandar)

--- _version.py
+++ _version.py
@@ -14,7 +14,7 @@ Pre-alpha. El pre-commit hook auto-bumpea según los cambios:
 import subprocess
 import os
 
-__version__ = "00.01.44"
+__version__ = "00.01.45"
 VERSION_TAG = f"v-{__version__}"
 
 
--- vision.py
+++ vision.py
@@ -192,6 +192,8 @@ def esperar_resultado_registro(ruta_obj_exito, ruta_obj_error, sector_region, ti
              if ubi_popup:
                  logger.info("-> EXITO! (pop-up confirmado)")
                  pyautogui.press('esc')
+                 time.sleep(0.3)
+                 pyautogui.press('enter')
                  time.sleep(0.5)
                  return 'exito'
         except pyautogui.ImageNotFoundException:
