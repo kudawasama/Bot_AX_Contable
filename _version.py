8b077e4633a8f553c5b164d4cabe9566c595f3dd
kudawasama <cespedes.jac@gmail.com>
2026-06-12 15:41:26 -0400

    fix: ajustar sector A solo a columna checkboxes (x=300, w=55)

--- _version.py
+++ _version.py
@@ -14,7 +14,7 @@ Pre-alpha. El pre-commit hook auto-bumpea según los cambios:
 import subprocess
 import os
 
-__version__ = "00.02.02"
+__version__ = "00.02.03"
 VERSION_TAG = f"v-{__version__}"
 
 
